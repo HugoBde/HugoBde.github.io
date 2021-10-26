c55991f497e69195b4e313b6e6ebbcbdafda0295
Hugo Bde <bouderliqueh@gmail.com>
2021-10-26 23:36:00 +1100

    WIP page

---
+++ public/scripts/modal.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById("modal")
+
+function closeModal() {
+    modal.classList.add("modalClosingAnimation")
+    setTimeout(() => modal.style.display = "none" ,500)
+}
+
+function showModal() {
+    modal.classList.add("modalOpeningAnimation")
+    setTimeout(() => modal.classList.remove("modalOpeningAnimation"), 750)
+}
+
+showModal()
