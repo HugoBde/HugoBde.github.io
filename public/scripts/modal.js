00eb7b2f82c5bbf8ab7fc2fe0158b3b6b370ba0f
Hugo Bde <bouderliqueh@gmail.com>
2021-10-27 14:37:23 +1100

    Bugfixes, chrome support

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,17 +1,34 @@
 const modal = document.getElementById("modal")
 
 function closeModal() {
-    document.body.style.overflow = ""
     modal.classList.add("modalFadeOut")
     setTimeout(() => modal.style.display = "none" ,700)
 }
 
 function showModal() {
-    document.body.style.overflow = "hidden"
-    window.scroll(0,0)
     modal.style.display = "flex"
     modal.classList.add("modalFadeIn")
     setTimeout(() => modal.classList.remove("modalFadeIn"), 750)
 }
 
+function getScrollTop() {
+    if (typeof window.pageYOffset !== "undefined" ) {
+        // Most browsers
+        return window.pageYOffset;
+    }
+  
+    let d = document.documentElement;
+    if (typeof d.clientHeight !== "undefined") {
+        // IE in standards mode
+        return d.scrollTop;
+    }
+  
+    // IE in quirks mode
+    return document.body.scrollTop;
+}
+
+window.onscroll = function() {
+    let scroll = getScrollTop();
+    modal.style.top = scroll + "px";
+};
 showModal()
