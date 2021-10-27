4f838db1232e935971e98cba8c9302a6a4663adb
Hugo Bde <bouderliqueh@gmail.com>
2021-10-27 13:58:24 +1100

    Some changes and placeholder

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,11 +1,14 @@
 const modal = document.getElementById("modal")
 
 function closeModal() {
+    document.body.style.overflow = ""
     modal.classList.add("modalFadeOut")
     setTimeout(() => modal.style.display = "none" ,700)
 }
 
 function showModal() {
+    document.body.style.overflow = "hidden"
+    window.scroll(0,0)
     modal.style.display = "flex"
     modal.classList.add("modalFadeIn")
     setTimeout(() => modal.classList.remove("modalFadeIn"), 750)
