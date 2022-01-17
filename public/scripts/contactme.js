1d3390ddb28c282d1c04a76e48a80882d556b5ea
Hugo Bouderlique <bouderliqueh@gmail.com>
2022-01-17 15:42:27 +1100

    Fixed another silly thing

--- public/scripts/contactme.js
+++ public/scripts/contactme.js
@@ -3,22 +3,21 @@ class OpenCloseButton {
         this.button = document.getElementById(idButton);
         this.form = document.getElementById(idForm);
         this.is_form_closed = true;
-        this.message_sent = false;
-        
+
         this.form.addEventListener("submit", function (event) {
             let send_button = document.getElementById("submit");
             setTimeout(() => {
                 send_button.innerHTML = "Sending..."
             }, 150);
             send_button.style.filter = "invert(100%)";
-            
+
             event.preventDefault();
             let form_data = new FormData(event.target);
             let payload = {};
             for (let [k, v] of form_data) {
                 payload[k] = v
             }
-            
+
             let xhr = new XMLHttpRequest();
             xhr.onload = function () {
                 setTimeout(() => {
@@ -26,13 +25,20 @@ class OpenCloseButton {
                 }, 150);
                 send_button.style.filter = "";
                 my_contact_form.message_sent = true;
+                if (this.status == 200) {
+                    setTimeout(() => {
+                        event.target.reset();
+                        send_button.innerHTML = "Send"
+                        my_contact_form.close_form();
+                    }, 3000);
+                }
             };
             xhr.open("POST", "https://8xmzncirgb.execute-api.ap-southeast-2.amazonaws.com/contactme");
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.send(JSON.stringify(payload));
         })
-        
-        
+
+
         this.switch_state = () => {
             if (this.is_form_closed) {
                 this.open_form();
@@ -40,19 +46,14 @@ class OpenCloseButton {
                 this.close_form();
             }
         }
-        
+
         this.close_form = () => {
             this.button.style.transform = "rotate(0deg)";
             this.is_form_closed = true;
             this.form.style.bottom = 55 - my_contact_form.form.offsetHeight + "px";
-            setTimeout(() => {
-                if (this.message_sent) {
-                    this.form.reset();
-                    this.send_button.innerHTML = "Send"
-                }
-            }, 500);
+
         }
-        
+
         this.open_form = () => {
             if (on_small_screen) {
                 this.form.scrollIntoView()
