bbbd917c09589966f9ba8302640928e931356cbc
Hugo Bouderlique <bouderliqueh@gmail.com>
2022-01-17 15:33:46 +1100

    Removed wip warning

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,13 +1,13 @@
-const modal = document.getElementById("modal");
+// const modal = document.getElementById("modal");
 
-function closeModal() {
-    modal.classList.add("modalFadeOut");
-    setTimeout(() => modal.style.display = "none" ,700);
-}
+// function closeModal() {
+//     modal.classList.add("modalFadeOut");
+//     setTimeout(() => modal.style.display = "none" ,700);
+// }
 
-function showModal() {
-    modal.classList.add("modalFadeIn");
-    setTimeout(() => modal.classList.remove("modalFadeIn"), 750);
-}
+// function showModal() {
+//     modal.classList.add("modalFadeIn");
+//     setTimeout(() => modal.classList.remove("modalFadeIn"), 750);
+// }
 
-showModal();
+// showModal();
